2535d0873ab3288b81751c89d4598f2bbc8eeb0c
song0152 <songshuxun5@gmail.com>
2025-08-08 19:24:06 -0400

    Update

--- game-reviews-frontend/src/router/index.js
+++ game-reviews-frontend/src/router/index.js
@@ -1,16 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../pages/Home.vue';
-import ReviewDetail from '../pages/ReviewDetail.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../pages/Home.vue'
+import ReviewDetail from '../pages/ReviewDetail.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
+  { path: '/game-reviews', redirect: '/' },        
   { path: '/platform/:platform', name: 'platform', component: Home, props: true },
   { path: '/review/:id', name: 'review', component: ReviewDetail, props: true },
-];
+  { path: '/:pathMatch(.*)*', redirect: '/' },     
+]
 
 export default createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),                         
   routes,
   scrollBehavior: () => ({ top: 0 }),
-});
+})
+
 
